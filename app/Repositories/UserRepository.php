388b757f105af0187f24742f09f899b8c55dea83
Arao Domingos <araodomingos04@gmail.com>
2024-07-21 00:17:35 +0100

    feat: implementando crud de posts e comentários

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -6,10 +6,6 @@ use App\Models\User;
 
 class UserRepository extends BaseRepository
 {
-    protected $relationships = [];
-    protected $filterable = ['status', 'role_id'];
-    protected $searchable = ['name', 'email'];
-
     public function __construct(User $user)
     {
         parent::__construct($user);
